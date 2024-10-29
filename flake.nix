fcdd5e2978d7c4f79c348e22188f1631d128335a
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-10-29 19:21:47 +0100

    Move to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "My NixOS config";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
