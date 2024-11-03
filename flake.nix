45b9ec503a3d02aa87395a71d3e9bde9b81786a1
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-11-03 21:07:03 +0100

    Initial setup of nix-darwin for m2

--- flake.nix
+++ flake.nix
@@ -10,12 +10,17 @@
       url = "github:javier-varez/nixvim-cfg";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     inputs@{
       home-manager,
       nixvim,
       nixpkgs,
+      nix-darwin,
       ...
     }:
     {
@@ -54,6 +59,18 @@
             home-manager.users.javier = import ./home/javier;
           }
 
+        ];
+      };
+
+      darwinConfigurations.m2 = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        specialArgs = {
+          inherit inputs;
+        };
+
+        modules = [
+          ./host/m2
+          nixvim.nixosModules.nixvim
         ];
       };
     };
