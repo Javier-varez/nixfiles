78488195439dc96be3154f0e460c14594857bb95
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-12-12 06:37:55 +0100

    Pin nixpkgs release 24.11

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "My NixOS config";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
