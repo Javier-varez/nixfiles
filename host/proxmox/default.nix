d4089ae4817f7e3217b833f2f1fa59d24d6c6764
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2025-09-03 23:46:22 +0200

    Jellyfin config

--- host/proxmox/default.nix
+++ host/proxmox/default.nix
@@ -2,16 +2,32 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ../common
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../common
+  ];
 
   networking.hostName = "proxmox";
+
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  environment.systemPackages = [
+    pkgs.jellyfin
+    pkgs.jellyfin-web
+    pkgs.jellyfin-ffmpeg
+  ];
 
   system.stateVersion = lib.mkForce "24.11";
 }
